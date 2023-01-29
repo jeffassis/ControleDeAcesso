e27bb76923470ed218d92dbb0a99d980ca1f55c5
jeffassis <cmdtjeff@gmail.com>
2023-01-29 13:04:16 -0300

    iniciando projeto

---
+++ ControleDeAcesso/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace ControleDeAcesso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cadastroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.faturamentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acessoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vendedoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fornecedoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.emitirNotaFiscalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cancelarNotaFiscalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultarNotaFiscalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultarFaturamentoDoMêsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comissãoDeVendedoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroDeUsuárioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.controleDeAcessoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inativarEAtivarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acessoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastroToolStripMenuItem,
+            this.faturamentoToolStripMenuItem,
+            this.acessoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastroToolStripMenuItem
+            // 
+            this.cadastroToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientesToolStripMenuItem,
+            this.vendedoresToolStripMenuItem,
+            this.fornecedoresToolStripMenuItem});
+            this.cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
+            this.cadastroToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.cadastroToolStripMenuItem.Text = "Cadastro ";
+            // 
+            // faturamentoToolStripMenuItem
+            // 
+            this.faturamentoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.emitirNotaFiscalToolStripMenuItem,
+            this.cancelarNotaFiscalToolStripMenuItem,
+            this.consultarNotaFiscalToolStripMenuItem,
+            this.consultarFaturamentoDoMêsToolStripMenuItem,
+            this.comissãoDeVendedoresToolStripMenuItem});
+            this.faturamentoToolStripMenuItem.Name = "faturamentoToolStripMenuItem";
+            this.faturamentoToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.faturamentoToolStripMenuItem.Text = "Faturamento";
+            // 
+            // acessoToolStripMenuItem
+            // 
+            this.acessoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastroDeUsuárioToolStripMenuItem,
+            this.controleDeAcessoToolStripMenuItem});
+            this.acessoToolStripMenuItem.Name = "acessoToolStripMenuItem";
+            this.acessoToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.acessoToolStripMenuItem.Text = "Acesso";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // vendedoresToolStripMenuItem
+            // 
+            this.vendedoresToolStripMenuItem.Name = "vendedoresToolStripMenuItem";
+            this.vendedoresToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.vendedoresToolStripMenuItem.Text = "Vendedores";
+            // 
+            // fornecedoresToolStripMenuItem
+            // 
+            this.fornecedoresToolStripMenuItem.Name = "fornecedoresToolStripMenuItem";
+            this.fornecedoresToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fornecedoresToolStripMenuItem.Text = "Fornecedores";
+            // 
+            // emitirNotaFiscalToolStripMenuItem
+            // 
+            this.emitirNotaFiscalToolStripMenuItem.Name = "emitirNotaFiscalToolStripMenuItem";
+            this.emitirNotaFiscalToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.emitirNotaFiscalToolStripMenuItem.Text = "Emitir Nota Fiscal";
+            // 
+            // cancelarNotaFiscalToolStripMenuItem
+            // 
+            this.cancelarNotaFiscalToolStripMenuItem.Name = "cancelarNotaFiscalToolStripMenuItem";
+            this.cancelarNotaFiscalToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.cancelarNotaFiscalToolStripMenuItem.Text = "Cancelar Nota Fiscal";
+            // 
+            // consultarNotaFiscalToolStripMenuItem
+            // 
+            this.consultarNotaFiscalToolStripMenuItem.Name = "consultarNotaFiscalToolStripMenuItem";
+            this.consultarNotaFiscalToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.consultarNotaFiscalToolStripMenuItem.Text = "Consultar Nota Fiscal";
+            // 
+            // consultarFaturamentoDoMêsToolStripMenuItem
+            // 
+            this.consultarFaturamentoDoMêsToolStripMenuItem.Name = "consultarFaturamentoDoMêsToolStripMenuItem";
+            this.consultarFaturamentoDoMêsToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.consultarFaturamentoDoMêsToolStripMenuItem.Text = "Consultar Faturamento do Mês";
+            // 
+            // comissãoDeVendedoresToolStripMenuItem
+            // 
+            this.comissãoDeVendedoresToolStripMenuItem.Name = "comissãoDeVendedoresToolStripMenuItem";
+            this.comissãoDeVendedoresToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.comissãoDeVendedoresToolStripMenuItem.Text = "Comissão de Vendedores";
+            // 
+            // cadastroDeUsuárioToolStripMenuItem
+            // 
+            this.cadastroDeUsuárioToolStripMenuItem.Name = "cadastroDeUsuárioToolStripMenuItem";
+            this.cadastroDeUsuárioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cadastroDeUsuárioToolStripMenuItem.Text = "Cadastro de Usuário";
+            // 
+            // controleDeAcessoToolStripMenuItem
+            // 
+            this.controleDeAcessoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inativarEAtivarToolStripMenuItem,
+            this.acessoToolStripMenuItem1});
+            this.controleDeAcessoToolStripMenuItem.Name = "controleDeAcessoToolStripMenuItem";
+            this.controleDeAcessoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.controleDeAcessoToolStripMenuItem.Text = "Controle de Acesso";
+            // 
+            // inativarEAtivarToolStripMenuItem
+            // 
+            this.inativarEAtivarToolStripMenuItem.Name = "inativarEAtivarToolStripMenuItem";
+            this.inativarEAtivarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inativarEAtivarToolStripMenuItem.Text = "Inativar e Ativar";
+            // 
+            // acessoToolStripMenuItem1
+            // 
+            this.acessoToolStripMenuItem1.Name = "acessoToolStripMenuItem1";
+            this.acessoToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.acessoToolStripMenuItem1.Text = "Acesso";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 93);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cadastroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem faturamentoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acessoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vendedoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fornecedoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem emitirNotaFiscalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cancelarNotaFiscalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultarNotaFiscalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultarFaturamentoDoMêsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comissãoDeVendedoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroDeUsuárioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem controleDeAcessoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inativarEAtivarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acessoToolStripMenuItem1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ControleDeAcesso/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleDeAcesso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(ToolStripMenuItem item in menuStrip1.Items)
+            {
+                MessageBox.Show(item.Text);
+            }
+        }
+    }
+}
